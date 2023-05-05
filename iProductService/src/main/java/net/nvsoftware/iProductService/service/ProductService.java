437cf1aa96e82fce17e0dcc4877760294561dcad
muyouqing <liuq915@gmail.com>
2023-05-04 23:35:28 -0700

    iPRoductService

--- iProductService/src/main/java/net/nvsoftware/iProductService/service/ProductService.java
+++
@@ -1,9 +0,0 @@
-package net.nvsoftware.iProductService.service;
-
-import net.nvsoftware.iProductService.model.ProductRequest;
-import net.nvsoftware.iProductService.model.ProductResponse;
-
-public interface ProductService {
-    public long addProduct(ProductRequest productRequest);
-    ProductResponse getProductById(long id);
-}
