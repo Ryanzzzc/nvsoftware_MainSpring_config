4e47cf447bb7d64b89d302a308c00bba3019ca73
muyouqing <liuq915@gmail.com>
2023-05-07 00:41:51 -0700

    doPayment API

--- iPaymentService/src/main/java/net/nvsoftware/iPaymentService/entity/PaymentEntity.java
+++ iPaymentService/src/main/java/net/nvsoftware/iPaymentService/entity/PaymentEntity.java
@@ -1,6 +1,7 @@
 package net.nvsoftware.iPaymentService.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,6 +12,7 @@ import java.time.Instant;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Builder
 public class PaymentEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
--- iPaymentService/src/main/java/net/nvsoftware/iPaymentService/service/PaymentService.java
+++ iPaymentService/src/main/java/net/nvsoftware/iPaymentService/service/PaymentService.java
@@ -1,7 +1,9 @@
 package net.nvsoftware.iPaymentService.service;
 
 import net.nvsoftware.iPaymentService.entity.PaymentEntity;
+import net.nvsoftware.iPaymentService.model.PaymentRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PaymentService{
+    long doPayment(PaymentRequest paymentRequest);
 }
