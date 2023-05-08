4b7a0ff27653ff9b95b023dd13f57018bf7e335f
muyouqing <liuq915@gmail.com>
2023-05-08 12:16:50 -0700

    PaymentService getPaymentByOrderId API

--- iPaymentService/src/main/java/net/nvsoftware/iPaymentService/service/PaymentService.java
+++ iPaymentService/src/main/java/net/nvsoftware/iPaymentService/service/PaymentService.java
@@ -2,8 +2,11 @@ package net.nvsoftware.iPaymentService.service;
 
 import net.nvsoftware.iPaymentService.entity.PaymentEntity;
 import net.nvsoftware.iPaymentService.model.PaymentRequest;
+import net.nvsoftware.iPaymentService.model.PaymentResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PaymentService{
     long doPayment(PaymentRequest paymentRequest);
+
+    PaymentResponse getPaymentByOrderId(long orderId);
 }
