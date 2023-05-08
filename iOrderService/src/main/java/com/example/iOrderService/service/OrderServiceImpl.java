61d875e5eb29939a6d95b26cf3c8390851f2306f
muyouqing <liuq915@gmail.com>
2023-05-08 11:45:09 -0700

    OrderService RestCall ProductService getProductById

--- iOrderService/src/main/java/com/example/iOrderService/service/OrderServiceImpl.java
+++ iOrderService/src/main/java/com/example/iOrderService/service/OrderServiceImpl.java
@@ -10,6 +10,7 @@ import com.example.iOrderService.repository.OrderRepository;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.time.Instant;
 
@@ -21,6 +22,8 @@ public class OrderServiceImpl implements OrderService{
     @Autowired
     private ProductServiceFeignClient productServiceFeignClient;
     private PaymentServiceFeignClient paymentServiceFeignClient;
+    @Autowired
+    private RestTemplate restTemplate;
     @Override
     public long placeOrder(OrderRequest orderRequest) {//TODO: make this method as transaction
         //call orderService(this) to create order entity with status CREATED, save to orderdb
@@ -70,18 +73,32 @@ public class OrderServiceImpl implements OrderService{
 
     @Override
     public OrderResponse getOrderById(long orderId) {
-        log.info("OrderService getOrderById start with orderId: " + orderId);
+        log.info("OrderService getOrderById start with orderId " +orderId);
         OrderEntity orderEntity = orderRepository.findById(orderId)
                 .orElseThrow(()->new RuntimeException("OrderService getOrderById NOT FOUND for: " + orderId));
+
+        // add productResponse
+        log.info("OrderService RestCall ProductService getByProductId " + orderEntity.getProductId());
+        OrderResponse.ProductResponse productResponse = restTemplate.getForObject(
+                "http://PRODUCT-SERVICE/products/"+orderEntity.getProductId(),
+                OrderResponse.ProductResponse.class
+        );
 
         OrderResponse orderResponse = OrderResponse.builder()
                 .orderId(orderEntity.getOrderId())
                 .totalAmount(orderEntity.getTotalAmount())
                 .orderDate(Instant.now())
                 .orderStatus(orderEntity.getOrderStatus())
+                .productResponse(productResponse)
                 .build();
+
+
+
+
         log.info("OrderService getOrderById done");
         return orderResponse;
 
     }
+
+
 }
