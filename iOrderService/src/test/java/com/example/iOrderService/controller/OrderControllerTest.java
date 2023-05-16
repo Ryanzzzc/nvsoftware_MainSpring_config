6a3c5e493da6f3053472ad9df9b6698973965dda
muyouqing <liuq915@gmail.com>
2023-05-16 16:16:15 -0700

    OrderServiceTest wiremock ControllerTest BeforeEach set up

--- iOrderService/src/test/java/com/example/iOrderService/controller/OrderControllerTest.java
+++ iOrderService/src/test/java/com/example/iOrderService/controller/OrderControllerTest.java
@@ -1,15 +1,99 @@
 package com.example.iOrderService.controller;
 
 import com.example.iOrderService.OrderServiceConfigTest;
+import com.example.iOrderService.repository.OrderRepository;
+import com.example.iOrderService.service.OrderService;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.StreamUtils;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
 
 @SpringBootTest
 @EnableConfigurationProperties
 @AutoConfigureMockMvc
 @ContextConfiguration(classes = {OrderServiceConfigTest.class})
 public class OrderControllerTest {
-        
+    @Autowired
+    private OrderService orderService;
+    @Autowired
+    private OrderRepository orderRepository;
+    @Autowired
+    private MockMvc mockMvc;
+
+    static WireMockExtension wireMockExtension = WireMockExtension.newInstance()
+            .options(WireMockConfiguration.wireMockConfig().port(8053))
+            .build();
+
+    @BeforeEach
+    void setupTest() throws IOException {  //put common method for every test
+        reduceQuantity();
+        getProductResponse();
+        doPayment();
+        getPaymentResponse();
+    }
+
+    private void getPaymentResponse() throws IOException {
+        wireMockExtension.stubFor( WireMock.get("/payments/1")
+                .willReturn(WireMock.aResponse()
+                        .withStatus(HttpStatus.OK.value())
+                        .withHeader("Content Type", MediaType.APPLICATION_JSON_VALUE)
+                        .withBody(StreamUtils.copyToString(OrderControllerTest.class.getClassLoader()
+                                .getResourceAsStream("MockData/paymentResponse.json"), Charset.defaultCharset())
+                        )
+                )
+        );
+    }
+
+    private void doPayment() {
+        wireMockExtension.stubFor(WireMock.post("/payments")
+                .willReturn(WireMock.aResponse()
+                        .withStatus(HttpStatus.OK.value())
+                        .withHeader("Content Type", MediaType.APPLICATION_JSON_VALUE)
+                )
+        );
+    }
+
+    private void getProductResponse() throws IOException {
+        wireMockExtension.stubFor( WireMock.get("/product/1")
+                .willReturn(WireMock.aResponse()
+                        .withStatus(HttpStatus.OK.value())
+                        .withHeader("Content Type", MediaType.APPLICATION_JSON_VALUE)
+                        .withBody(StreamUtils.copyToString(OrderControllerTest.class.getClassLoader()
+                                .getResourceAsStream("MockData/productResponse.json"), Charset.defaultCharset())
+                                )
+                        )
+        );
+
+    }
+
+    private void reduceQuantity() {
+        wireMockExtension.stubFor(WireMock.put("/products/reduceQuantity")
+                .willReturn(WireMock.aResponse()
+                        .withStatus(HttpStatus.OK.value())
+                        .withHeader("Content Type", MediaType.APPLICATION_JSON_VALUE)
+                )
+        );
+    }
+
+    @Test
+    public void testPlaceOrderDoPaymentSuccess() { //TODO FINISH JWT TOKEN FOR SERVICES
+        //Place Order
+        //Get Order from DB by orderId
+        //verify orderResponse
+
+    }
+
 }
